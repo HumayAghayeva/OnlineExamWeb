764ae83f33e8d70db8f198c633ef99df919a4a0c
humayra <humayra@code.edu.az>
2024-06-26 16:20:45 +0400

    Add project files.

---
+++ OnlineExamWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineExamWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
