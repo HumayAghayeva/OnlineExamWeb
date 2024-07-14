8282b62997eb2376b42184d0dd90bd0cac35915b
humayra <humayra@code.edu.az>
2024-07-14 16:37:00 +0400

    DEV-6:  Added log

--- OnlineExamWeb/Program.cs
+++ OnlineExamWeb/Program.cs
@@ -1,3 +1,9 @@
+using Infrastructure.DataContext.Write;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,16 +18,47 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+builder.Logging.AddConsole();
+var logger =NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+try
+{
+    logger.Info("init main");
+    /************************************************
+     * Add connection string to services container - using EF pooling for performance
+     ************************************************/
+    builder.Services.AddDbContext<DBConn>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("WriteDbContext"), builder =>
+        {
+            builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+        });
+        options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    });
 
-app.UseRouting();
 
-app.UseAuthorization();
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.UseRouting();
 
-app.Run();
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+
+}
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
