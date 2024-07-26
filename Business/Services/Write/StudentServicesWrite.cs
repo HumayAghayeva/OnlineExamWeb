69c4e396ef4b264c0f2c5bdca94da80266ff9bc3
Humay Aghayeva <HAghayeva@pasha-insurance.az>
2024-07-26 23:11:46 +0400

    dev-13: crud operations

--- Business/Services/Write/StudentServicesWrite.cs
+++ Business/Services/Write/StudentServicesWrite.cs
@@ -9,15 +9,21 @@ using System.Threading.Tasks;
 using Domain.Models;
 using Domain.Enums;
 using System.Data;
+using Domain.DTOs.Read;
+using Azure.Core;
+using static Azure.Core.HttpHeader;
+using AutoMapper;
 
 namespace Business.Services.Write
 {
     public class StudentServicesWrite : IStudentPostRepository
     {
         private readonly IUnitOfWork uow;
-        public StudentServicesWrite(IUnitOfWork _uow)
+        private readonly IMapper mapper;
+        public StudentServicesWrite(IUnitOfWork _uow,IMapper _mapper)
         {
-            uow = _uow;
+              uow = _uow ?? throw new ArgumentNullException(nameof(_uow));
+              mapper = _mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
         public async Task PostStudentAsync(StudentRequestDTO studentRequestDTO, CancellationToken token)
         {
@@ -25,18 +31,14 @@ namespace Business.Services.Write
             {
                 await uow.BeginTransactionAsync(IsolationLevel.ReadCommitted, token);
 
-                var student = new Student
-                {
-                    Name = studentRequestDTO.Name,
-                    LastName = studentRequestDTO.LastName,
-                    DateOfBirth = studentRequestDTO.DateOfBirth,
-                    GroupId = studentRequestDTO.GroupId
-                };
-
+                var student = mapper.Map<Student>(studentRequestDTO);
+              
                 var studentRepository = uow.Repository<Student>();
+
                 studentRepository.Add(student);
 
                 await uow.SaveAsync(token);
+
                 await uow.CommitAsync(token);
             }
             catch (Exception ex)
@@ -46,9 +48,52 @@ namespace Business.Services.Write
             }
         }
 
-        public async void GetStudentById(int id, CancellationToken token)
+        public async Task<StudentReadDTO> GetStudentById(int id, CancellationToken token)
         {
+            var studentRepository =  uow.Repository<Student>();       
 
+            var student= await studentRepository.GetByIdAsync(id);
+
+            var studentResponse = mapper.Map<StudentReadDTO>(student);
+
+            if (studentResponse == null)
+            {
+                return  null;
+            }
+
+            return studentResponse;
+
+        }
+        public async Task UpdateStudentAsync(int id, StudentRequestDTO studentRequest, CancellationToken token)
+        {
+            try
+            {
+                await uow.BeginTransactionAsync(IsolationLevel.ReadCommitted, token);
+
+                var studentRepository = uow.Repository<Student>();
+
+                var existingStudent = await studentRepository.GetByIdAsync(id);
+
+                if (existingStudent == null)
+                {
+                    throw new KeyNotFoundException($"Student with id {id} not found.");
+                }
+
+              
+                mapper.Map(studentRequest, existingStudent);
+
+                studentRepository.Update(existingStudent);
+
+                await uow.SaveAsync(token);
+                await uow.CommitAsync(token);
+            }
+            catch (Exception ex)
+            {
+                await uow.RollbackAsync(token);
+
+                throw;
+
+            }
         }
     }
 }
