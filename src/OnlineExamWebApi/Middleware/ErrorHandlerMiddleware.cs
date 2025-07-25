6f10d962dc4846f4e084e0544dde5a4a881eaa09
humayra <humayra@code.edu.az>
2025-07-25 18:46:49 +0400

    Added HttpHeadersMiddleware middleware

---
+++ src/OnlineExamWebApi/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Contract;
+using Domain.Enums;
+using System.Net;
+using System.Text.Json;
+
+namespace OnlineExamWebApi.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            ApiResponse apiResponse = new ApiResponse();
+            if (error is GlobalException)
+            {
+                GlobalException exception = (GlobalException)error;
+                apiResponse.Code = exception.Code;
+                apiResponse.Message = exception.Message;
+            }
+            else
+            {
+                apiResponse.Code = ResponseCode.InternalServerError;
+                apiResponse.Message = error.Message;
+            }
+            var result = JsonSerializer.Serialize(apiResponse, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+            });
+
+            return response.WriteAsync(result);
+        }
+    }
+}
+
---
+++ src/OnlineExamWebApi/Middleware/HttpHeadersMiddleware.cs
@@ -0,0 +1,61 @@
+﻿namespace OnlineExamWebApi.Middleware
+{
+    public sealed class HttpHeadersMiddleware
+    {
+        /// <summary>
+        /// The delegate for the next part of the pipeline. This field is read-only.
+        /// </summary>
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpHeadersMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">The delegate for the next part of the pipeline.</param>
+        public HttpHeadersMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        /// Invokes the middleware asynchronously.
+        /// </summary>
+        /// <param name="context">The current HTTP context.</param>
+        /// <returns>
+        /// A <see cref="Task"/> representing the actions performed by the middleware.
+        /// </returns>
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.OnStarting(async () =>
+            {
+                context.Response.Headers.Remove("Server");
+                context.Response.Headers.Remove("X-Powered-By");
+
+                context.Response.Headers.Remove("Referrer-Policy");
+                context.Response.Headers.Remove("X-Content-Type-Options");
+                context.Response.Headers.Remove("X-Frame-Options");
+                context.Response.Headers.Remove("X-XSS-Protection");
+                context.Response.Headers.Remove("X-Request-Id");
+                context.Response.Headers.Remove("X-Request-Duration");
+
+                context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add("X-Frame-Options", "DENY");
+                context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
+
+                context.Response.Headers.Add("X-Request-Id", context.TraceIdentifier);
+
+
+            });
+
+            await _next(context).ConfigureAwait(false);
+        }
+    }
+
+    public static class HttpHeadersMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHttpHeadersMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HttpHeadersMiddleware>();
+        }
+    }
+}
