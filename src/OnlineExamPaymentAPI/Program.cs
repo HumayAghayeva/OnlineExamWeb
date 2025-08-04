5212ccbf8c2209edbc89a974742b5295037d5e95
humayra <humayra@code.edu.az>
2025-08-04 10:13:18 +0400

    Added

--- src/OnlineExamPaymentAPI/Program.cs
+++ src/OnlineExamPaymentAPI/Program.cs
@@ -1,6 +1,7 @@
 using Abstraction.PaymentApi.Interfaces.CardOperations;
 using OnlineExamPaymentAPI.Dtos.Request;
 using OnlineExamPaymentAPI.Endpoints;
+using OnlineExamPaymentAPI.Services.PaymentApiServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<ICardValidator, CardValidatorServices>(); // Make sure this class exists
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
